326c7b24b26ac0507f2c6b8d6ed0816c54dd439c
Josh <1755797+joshwyant@users.noreply.github.com>
2025-03-31 00:43:04 -0700

    Expanding word sizes to accomodate later gen processors

--- Emulate8086.Processor/CPU-registers.cs
+++ Emulate8086.Processor/CPU-registers.cs
@@ -4,8 +4,82 @@ namespace Emulate8086.Processor
 {
     public partial class CPU
     {
-        ushort[] register_file = new ushort[8];
-        ushort[] segment_register_file = new ushort[4];
+        const int Bits16WordSize = 2;
+        const int Bits32WordSize = 4;
+        const int Bits64WordSize = 8;
+        const ulong Bits16Mask = 0x00000000_0000FFFF;
+        const ulong Bits20Mask = 0x00000000_000F_FFFF;
+        const ulong Bit20thMask = 0x00000000_0010_0000;
+        const ulong Bits32Mask = 0x00000000_FFFFFFFF;
+        const ulong Bits52Mask = 0x000F_FFFF_FFFF_FFFF;
+        const ulong Bits57Mask = 0x01FFFFFF_FFFFFFFF;
+        const ulong Bits64Mask = 0xFFFF_FFFF_FFFF_FFFF;
+        public int WordSize { get; protected set; } = Bits16WordSize;
+        public int PointerSize { get; set; } = Bits16WordSize;
+        public int DefaultOperandSize { get; protected set; } = Bits16WordSize;
+        public int AlternateOperandSize { get; protected set; } = Bits32WordSize;
+        private ulong word_mask = Bits16Mask;
+        private ulong default_operand_mask = Bits32Mask;
+        private ulong alternate_operand_mask = Bits32Mask;
+        private ulong physical_address_mask = Bits20Mask; // 1mb; a20 disabled
+        private ulong virtual_address_mask = Bits20Mask;
+        private bool rexW = false;
+        ulong[] register_file = new ulong[16]; // including 64-bit extra
+        ulong[] segment_register_file = new ulong[6]; // Including FS and GS
+        bool a20enabled = false;
+        public void EnableA20(bool enable)
+        {
+            if (a20enabled = enable) // assignment, not equality
+            {
+                physical_address_mask |= Bit20thMask;
+                virtual_address_mask |= Bit20thMask;
+            }
+            else
+            {
+
+                physical_address_mask &= ~Bit20thMask;
+                virtual_address_mask &= ~Bit20thMask;
+            }
+        }
+        public void Set16BitWordSize()
+        {
+            WordSize = Bits16WordSize;
+            DefaultOperandSize = Bits16WordSize;
+            AlternateOperandSize = Bits32WordSize;
+            PointerSize = Bits16WordSize;
+            word_mask = Bits16Mask;
+            default_operand_mask = Bits16Mask;
+            alternate_operand_mask = Bits32Mask;
+            physical_address_mask = Bits20Mask; // a20 disabled
+            virtual_address_mask = Bits20Mask;
+            EnableA20(a20enabled);
+        }
+        public void Set32BitWordSize(bool pae = true)
+        {
+            WordSize = Bits32WordSize;
+            DefaultOperandSize = Bits32WordSize;
+            AlternateOperandSize = Bits16WordSize;
+            PointerSize = Bits32WordSize;
+            word_mask = Bits32Mask;
+            default_operand_mask = Bits32Mask;
+            alternate_operand_mask = Bits16Mask;
+            physical_address_mask = pae ? Bits52Mask : Bits32Mask; // 52 bits
+            virtual_address_mask = Bits32Mask;
+            EnableA20(a20enabled);
+        }
+        public void Set64BitWordSize()
+        {
+            WordSize = Bits64WordSize;
+            DefaultOperandSize = Bits32WordSize;
+            AlternateOperandSize = Bits16WordSize;
+            PointerSize = Bits64WordSize;
+            word_mask = Bits64Mask;
+            default_operand_mask = Bits32Mask;
+            alternate_operand_mask = Bits16Mask;
+            physical_address_mask = Bits57Mask; // 57 bits
+            virtual_address_mask = Bits64Mask;
+            EnableA20(a20enabled);
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ushort GetReg(Register r, bool word)
@@ -31,13 +105,13 @@ namespace Emulate8086.Processor
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ushort GetReg16(Register r)
         {
-            return register_file[(int)r];
+            return (ushort)register_file[(int)r];
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ushort GetSeg(Register r)
         {
-            return segment_register_file[(int)r];
+            return (ushort)segment_register_file[(int)r];
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -65,7 +139,7 @@ namespace Emulate8086.Processor
             // Index into ax/bx/cx/dx.
             var i = (int)r & 0b11;
             // Get ax/bx/cx/dx
-            var s = register_file[i];
+            var s = (ushort)register_file[i];
             // Do we want high or low byte?
             var is_low = ((int)r & 0b100) == 0;
             if (is_low)
@@ -80,10 +154,10 @@ namespace Emulate8086.Processor
                 // by masking the low byte
                 // and shifting the new value to
                 // the higher byte
-                s = (ushort)((s & 0x00FF) | (val << 8));
+                s = (ushort)((ushort)(s & 0x00FF) | (ushort)(val << 8));
             }
             // Replace the register with the new value
-            register_file[i] = s;
+            register_file[i] = register_file[i] & 0xFFFFFFFF_FFFF0000 | s;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -106,9 +180,9 @@ namespace Emulate8086.Processor
         ushort ax
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => register_file[0];
+            get => (ushort)register_file[0];
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => register_file[0] = value;
+            set => register_file[0] = register_file[0] & 0xFFFFFFFF_FFFF0000 | value;
         }
 
         byte al
@@ -118,7 +192,7 @@ namespace Emulate8086.Processor
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             set
             {
-                register_file[0] &= 0xFF00;
+                register_file[0] &= 0xFFFFFFFF_FFFFFF00;
                 register_file[0] |= value;
             }
         }
@@ -129,7 +203,7 @@ namespace Emulate8086.Processor
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             set
             {
-                register_file[0] &= 0x00FF;
+                register_file[0] &= 0xFFFFFFFF_FFFF00FF;
                 register_file[0] |= (ushort)(value << 8);
             }
         }
@@ -141,9 +215,9 @@ namespace Emulate8086.Processor
         ushort cx
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => register_file[1];
+            get => (ushort)register_file[1];
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => register_file[1] = value;
+            set => register_file[1] = register_file[1] & 0xFFFFFFFF_FFFF0000 | value;
         }
 
         byte cl
@@ -153,7 +227,7 @@ namespace Emulate8086.Processor
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             set
             {
-                register_file[1] &= 0xFF00;
+                register_file[1] &= 0xFFFFFFFF_FFFFFF00;
                 register_file[1] |= value;
             }
         }
@@ -164,7 +238,7 @@ namespace Emulate8086.Processor
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             set
             {
-                register_file[1] &= 0x00FF;
+                register_file[1] &= 0xFFFFFFFF_FFFF00FF;
                 register_file[1] |= (ushort)(value << 8);
             }
         }
@@ -176,9 +250,9 @@ namespace Emulate8086.Processor
         ushort dx
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => register_file[2];
+            get => (ushort)register_file[2];
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => register_file[2] = value;
+            set => register_file[2] = register_file[2] & 0xFFFFFFFF_FFFF0000 | value;
         }
 
         byte dl
@@ -188,7 +262,7 @@ namespace Emulate8086.Processor
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             set
             {
-                register_file[2] &= 0xFF00;
+                register_file[2] &= 0xFFFFFFFF_FFFFFF00;
                 register_file[2] |= value;
             }
         }
@@ -199,7 +273,7 @@ namespace Emulate8086.Processor
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             set
             {
-                register_file[2] &= 0x00FF;
+                register_file[2] &= 0xFFFFFFFF_FFFF00FF;
                 register_file[2] |= (ushort)(value << 8);
             }
         }
@@ -211,9 +285,9 @@ namespace Emulate8086.Processor
         ushort bx
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => register_file[3];
+            get => (ushort)register_file[3];
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => register_file[3] = value;
+            set => register_file[3] = register_file[3] & 0xFFFFFFFF_FFFF0000 | value;
         }
 
         byte bl
@@ -223,7 +297,7 @@ namespace Emulate8086.Processor
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             set
             {
-                register_file[3] &= 0xFF00;
+                register_file[3] &= 0xFFFFFFFF_FFFFFF00;
                 register_file[3] |= value;
             }
         }
@@ -234,7 +308,7 @@ namespace Emulate8086.Processor
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             set
             {
-                register_file[3] &= 0x00FF;
+                register_file[3] &= 0xFFFFFFFF_FFFF00FF;
                 register_file[3] |= (ushort)(value << 8);
             }
         }
@@ -242,65 +316,65 @@ namespace Emulate8086.Processor
         ushort sp
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => register_file[4];
+            get => (ushort)register_file[4];
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => register_file[4] = value;
+            set => register_file[4] = register_file[4] & 0xFFFFFFFF_FFFF0000 | value;
         }
 
         ushort bp
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => register_file[5];
+            get => (ushort)register_file[5];
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => register_file[5] = value;
+            set => register_file[5] = register_file[5] & 0xFFFFFFFF_FFFF0000 | value;
         }
 
         ushort si
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => register_file[6];
+            get => (ushort)register_file[6];
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => register_file[6] = value;
+            set => register_file[6] = register_file[6] & 0xFFFFFFFF_FFFF0000 | value;
         }
 
         ushort di
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => register_file[7];
+            get => (ushort)register_file[7];
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => register_file[7] = value;
+            set => register_file[7] = register_file[7] & 0xFFFFFFFF_FFFF0000 | value;
         }
 
         ushort es
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => segment_register_file[0];
+            get => (ushort)segment_register_file[0];
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => segment_register_file[0] = value;
+            set => segment_register_file[0] = segment_register_file[0] & 0xFFFFFFFF_FFFF0000 | value;
         }
 
         ushort cs
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => segment_register_file[1];
+            get => (ushort)segment_register_file[1];
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => segment_register_file[1] = value;
+            set => segment_register_file[1] = segment_register_file[1] & 0xFFFFFFFF_FFFF0000 | value;
         }
 
         ushort ss
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => segment_register_file[2];
+            get => (ushort)segment_register_file[2];
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => segment_register_file[2] = value;
+            set => segment_register_file[2] = segment_register_file[2] & 0xFFFFFFFF_FFFF0000 | value;
         }
 
         ushort ds
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => segment_register_file[3];
+            get => (ushort)segment_register_file[3];
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => segment_register_file[3] = value;
+            set => segment_register_file[3] = segment_register_file[3] & 0xFFFFFFFF_FFFF0000 | value;
         }
 
         ushort ip;
